d304f750afd474c79143577e2cdb8abb00e163bd
HiggsBosonic <natstonecipher@gmail.com>
2021-10-24 12:42:34 -0400

    Added D2 chars

--- scripts/run.js
+++ scripts/run.js
@@ -1,12 +1,17 @@
 const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
     const gameContract = await gameContractFactory.deploy(
-      ["Leo", "Aang", "Pikachu"],       // Names
-      ["https://i.imgur.com/pKd5Sdk.png", // Images
-      "https://i.imgur.com/xVu4vFL.png", 
-      "https://i.imgur.com/WMB6g9u.png"],
-      [100, 200, 300],                    // HP values
-      [100, 50, 25]                       // Attack damage values
+      ["Amazon", "Barbarian", "Assasin", "Sorceress", "Paladin", "Necromancer", "Druid"],       // Names
+      ["https://static.wikia.nocookie.net/diablo_gamepedia/images/9/9c/Amazon.gif/revision/latest?cb=20090419123107", // Images
+      "https://static.wikia.nocookie.net/diablo_gamepedia/images/a/a7/Barbarian_diablo_II.gif/revision/latest?cb=20090419123430", 
+      "https://static.wikia.nocookie.net/diablo_gamepedia/images/3/37/Assassin.gif/revision/latest?cb=20090419123240",
+      "https://static.wikia.nocookie.net/diablo_gamepedia/images/f/f2/Sorceress.gif/revision/latest?cb=20090419123520",
+      "https://static.wikia.nocookie.net/diablo_gamepedia/images/c/c3/Paladin.gif/revision/latest?cb=20090419123703",
+      "https://static.wikia.nocookie.net/diablo_gamepedia/images/7/73/Necromancer.gif/revision/latest?cb=20090419123323",
+      "https://static.wikia.nocookie.net/diablo_gamepedia/images/a/a8/Druid.gif/revision/latest?cb=20090419123624"
+      ],
+      [150, 500, 300, 150, 300, 200, 400],                    // HP values
+      [100, 25, 50, 100, 50, 75, 35]                       // Attack damage values
     );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
