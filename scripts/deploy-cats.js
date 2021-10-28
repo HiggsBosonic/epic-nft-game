38cef3ae6d47540459194d8872d66b0bc1795cc3
HiggsBosonic <natstonecipher@gmail.com>
2021-10-28 14:23:21 -0400

    cat deploy

--- scripts/deploy-cats.js
+++ scripts/deploy-cats.js
@@ -3,19 +3,18 @@
   const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
     const gameContract = await gameContractFactory.deploy(
-        ["Sus Cat", "Street Cat", "Sleepy Cat", "Chill Ginger", "Seen Some Things Cat", "Eyes Cat", "Kitten Gang"],       // Names
-        ["https://cdn.pixabay.com/photo/2020/09/27/09/34/cats-5606202__340.jpg", // Images
-        "https://images.squarespace-cdn.com/content/v1/5ed560580ac03e4f6cc34215/1596072868235-0Y0JFC4YTNPAFLK73RUT/What+Happens+When+You+Feed+Feral+Cats_Article+Photo_01.jpeg", 
-        "https://images.hindustantimes.com/rf/image_size_630x354/HT/p2/2020/03/03/Pictures/_4419b1ea-5d2b-11ea-be04-6cf974e2d9f7.jpg",
-        "https://meanderingwild.com/wp-content/uploads/2018/05/marrakech-cat-ginger.jpg",
-        "https://semfgnqp2z.exactdn.com/wp-content/uploads/2017/04/feral-cat.jpg?strip=all&lossy=1&ssl=1",
-        "https://s30379.pcdn.co/wp-content/uploads/2019/09/p1cuv198jk1t3oief1vesoae1rnq6.jpg",
-        "https://static.toiimg.com/photo/80225221.cms"],
-        [150, 500, 300, 150, 300, 200, 400],                    // HP values
-        [100, 25, 50, 100, 50, 75, 35],                       // Attack damage values
-        "Racooon", // Boss name
-        "https://images.dailyhive.com/20200603152104/shutterstock_1172774881.jpg", // Boss image
-        10000, // Boss hp
+        ["Sus", "Chill Ginger", "Orange Duo", "Eyes Cat", "Rach Bro's"],       // Names
+        ["https://imgur.com/UXcZH3q",
+        "https://imgur.com/n23oH24",
+        "https://imgur.com/jZ0ZDtq",
+        "https://imgur.com/IGw3hwD",
+        "https://imgur.com/2EXdZdJ",
+      ],
+        [150, 500, 300, 150, 400],                    // HP values
+        [100, 25, 50, 100, 35],                       // Attack damage values
+        "Racooon Boss", // Boss name
+        "https://imgur.com/G5BiEQm", // Boss image
+        500, // Boss hp
         50 // Boss attack damage
       );
     await gameContract.deployed();
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,21 +3,20 @@
   const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
     const gameContract = await gameContractFactory.deploy(
-        ["Amazon", "Barbarian", "Assasin", "Sorceress", "Paladin", "Necromancer", "Druid"],       // Names
-        ["https://static.wikia.nocookie.net/diablo_gamepedia/images/9/9c/Amazon.gif/revision/latest?cb=20090419123107", // Images
-        "https://static.wikia.nocookie.net/diablo_gamepedia/images/a/a7/Barbarian_diablo_II.gif/revision/latest?cb=20090419123430", 
-        "https://static.wikia.nocookie.net/diablo_gamepedia/images/3/37/Assassin.gif/revision/latest?cb=20090419123240",
-        "https://static.wikia.nocookie.net/diablo_gamepedia/images/f/f2/Sorceress.gif/revision/latest?cb=20090419123520",
-        "https://static.wikia.nocookie.net/diablo_gamepedia/images/c/c3/Paladin.gif/revision/latest?cb=20090419123703",
-        "https://static.wikia.nocookie.net/diablo_gamepedia/images/7/73/Necromancer.gif/revision/latest?cb=20090419123323",
-        "https://static.wikia.nocookie.net/diablo_gamepedia/images/a/a8/Druid.gif/revision/latest?cb=20090419123624"],
-        [150, 500, 300, 150, 300, 200, 400],                    // HP values
-        [100, 25, 50, 100, 50, 75, 35],                       // Attack damage values
-        "Diablo", // Boss name
-        "https://static.wikia.nocookie.net/diablo_gamepedia/images/9/92/Diablo_%28Diablo_II%29.gif/revision/latest/scale-to-width-down/135?cb=20090517132643", // Boss image
-        10000, // Boss hp
-        50 // Boss attack damage
-      );
+      ["Sus", "Chill Ginger", "Orange Duo", "Eyes Cat", "Rach Bro's"],       // Names
+      ["https://imgur.com/UXcZH3q",
+      "https://imgur.com/n23oH24",
+      "https://imgur.com/jZ0ZDtq",
+      "https://imgur.com/IGw3hwD",
+      "https://imgur.com/2EXdZdJ",
+    ],
+      [150, 500, 300, 150, 400],                    // HP values
+      [100, 25, 50, 100, 35],                       // Attack damage values
+      "Racooon Boss", // Boss name
+      "https://imgur.com/G5BiEQm", // Boss image
+      500, // Boss hp
+      50 // Boss attack damage
+    );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
   
